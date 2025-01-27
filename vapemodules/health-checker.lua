84c257237548065a80b1c780bc5d58ae6959d2cf
nxyq <cdn.pkplaysrblx@gmail.com>
2025-01-27 17:05:47 +0000

    yes

--- vapemodules/health-checker.lua
+++ vapemodules/health-checker.lua
@@ -7,8 +7,9 @@ local hc = vape.Categories.Combat:CreateModule({
           	local hum = game.Players.LocalPlayer.Character.Humanoid
 			local he = hum.Health
 			if he < 50 then
-				createwarning("HealthChecker", "Your health has been detected as lower than 50!")
+				vape:CreateNotification('HealthChecker', 'Your health is below 50.', 5, 'alert')
 			end
     end,
+    ExtraText = function() return 'Healthseek' end,
     Tooltip = 'Checks your health if it is nearly 0.'
 })
