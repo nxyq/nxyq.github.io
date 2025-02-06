4dec79a0afdaf65b823c2a0bdef65f159b39bf72
nxyq <cdn.pkplaysrblx@gmail.com>
2025-02-06 19:44:57 +0000

    New

---
+++ pkvxpeSessionInfo.lua
@@ -0,0 +1,215 @@
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+]=]
+
+-- Instances: 19 | Scripts: 4 | Modules: 0 | Tags: 0
+local pkvxpeSessionInfo = {};
+
+-- StarterGui.pkvxpeNewSessionInfo
+pkvxpeSessionInfo["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+pkvxpeSessionInfo["1"]["Name"] = [[pkvxpeNewSessionInfo]];
+pkvxpeSessionInfo["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame
+pkvxpeSessionInfo["2"] = Instance.new("Frame", pkvxpeSessionInfo["1"]);
+pkvxpeSessionInfo["2"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["2"]["BackgroundColor3"] = Color3.fromRGB(41, 41, 41);
+pkvxpeSessionInfo["2"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+pkvxpeSessionInfo["2"]["Size"] = UDim2.new(0.30507, 0, 0.3109, 0);
+pkvxpeSessionInfo["2"]["Position"] = UDim2.new(0.83425, 0, 0.18733, 0);
+pkvxpeSessionInfo["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.ImageLabel
+pkvxpeSessionInfo["3"] = Instance.new("ImageLabel", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["3"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+if getgenv().Image == "Vape" then
+	pkvxpeSessionInfo["3"]["Image"] = [[rbxassetid://14373395239]];
+elseif getgenv().Image == "Player"  then
+	local Player = game.Players.LocalPlayer
+	local ImageSize = Enum.ThumbnailSize.Size420x420
+	local ImageType = Enum.ThumbnailType.HeadShot
+	local content = game.Players:GetUserThumbnailAsync(Player.UserId, ImageType, ImageSize)
+	pkvxpeSessionInfo["3"]["Image"] = content
+else
+	pkvxpeSessionInfo["3"]["Image"] = [[rbxassetid://96650947896291]]
+end
+pkvxpeSessionInfo["3"]["Size"] = UDim2.new(0.19856, 0, 0.28351, 0);
+pkvxpeSessionInfo["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["3"]["BackgroundTransparency"] = 1;
+pkvxpeSessionInfo["3"]["Position"] = UDim2.new(0.03249, 0, 0.04639, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.ImageLabel.UIAspectRatioConstraint
+pkvxpeSessionInfo["4"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["3"]);
+pkvxpeSessionInfo["4"]["AspectRatio"] = 1;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrDisplay
+pkvxpeSessionInfo["5"] = Instance.new("TextLabel", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["5"]["TextWrapped"] = true;
+pkvxpeSessionInfo["5"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["5"]["TextSize"] = 14;
+pkvxpeSessionInfo["5"]["TextScaled"] = true;
+pkvxpeSessionInfo["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+pkvxpeSessionInfo["5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["5"]["BackgroundTransparency"] = 1;
+pkvxpeSessionInfo["5"]["Size"] = UDim2.new(0.58123, 0, 0.13402, 0);
+pkvxpeSessionInfo["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["5"]["Text"] = [[pkplaysrblx]];
+pkvxpeSessionInfo["5"]["Name"] = [[plrDisplay]];
+pkvxpeSessionInfo["5"]["Position"] = UDim2.new(0.32491, 0, 0.04639, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrDisplay.LocalScript
+pkvxpeSessionInfo["6"] = Instance.new("LocalScript", pkvxpeSessionInfo["5"]);
+
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrDisplay.UIAspectRatioConstraint
+pkvxpeSessionInfo["7"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["5"]);
+pkvxpeSessionInfo["7"]["AspectRatio"] = 6.19231;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrName
+pkvxpeSessionInfo["8"] = Instance.new("TextLabel", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["8"]["TextWrapped"] = true;
+pkvxpeSessionInfo["8"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["8"]["TextSize"] = 14;
+pkvxpeSessionInfo["8"]["TextScaled"] = true;
+pkvxpeSessionInfo["8"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+pkvxpeSessionInfo["8"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["8"]["BackgroundTransparency"] = 1;
+pkvxpeSessionInfo["8"]["Size"] = UDim2.new(0.58123, 0, 0.13402, 0);
+pkvxpeSessionInfo["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["8"]["Text"] = [[@pkplaysrblx]];
+pkvxpeSessionInfo["8"]["Name"] = [[plrName]];
+pkvxpeSessionInfo["8"]["Position"] = UDim2.new(0.32491, 0, 0.18041, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrName.LocalScript
+pkvxpeSessionInfo["9"] = Instance.new("LocalScript", pkvxpeSessionInfo["8"]);
+
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrName.UIAspectRatioConstraint
+pkvxpeSessionInfo["a"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["8"]);
+pkvxpeSessionInfo["a"]["AspectRatio"] = 6.19231;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.Frame
+pkvxpeSessionInfo["b"] = Instance.new("Frame", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["b"]["ZIndex"] = 0;
+pkvxpeSessionInfo["b"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["b"]["BackgroundColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["b"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+pkvxpeSessionInfo["b"]["Size"] = UDim2.new(1, 0, 1, 0);
+pkvxpeSessionInfo["b"]["Position"] = UDim2.new(0.49851, 0, 0.49506, 0);
+pkvxpeSessionInfo["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["b"]["BackgroundTransparency"] = 0.4;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.Frame.UIAspectRatioConstraint
+pkvxpeSessionInfo["c"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["b"]);
+pkvxpeSessionInfo["c"]["AspectRatio"] = 1.42784;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentTime
+pkvxpeSessionInfo["d"] = Instance.new("TextLabel", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["d"]["TextWrapped"] = true;
+pkvxpeSessionInfo["d"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["d"]["TextSize"] = 14;
+pkvxpeSessionInfo["d"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+pkvxpeSessionInfo["d"]["TextScaled"] = true;
+pkvxpeSessionInfo["d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["d"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+pkvxpeSessionInfo["d"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["d"]["BackgroundTransparency"] = 1;
+pkvxpeSessionInfo["d"]["Size"] = UDim2.new(0.9639, 0, 0.13402, 0);
+pkvxpeSessionInfo["d"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["d"]["Text"] = [[Time:]];
+pkvxpeSessionInfo["d"]["Name"] = [[currentTime]];
+pkvxpeSessionInfo["d"]["Position"] = UDim2.new(0.03249, 0, 0.42784, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentTime.LocalScript
+pkvxpeSessionInfo["e"] = Instance.new("LocalScript", pkvxpeSessionInfo["d"]);
+
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentTime.UIAspectRatioConstraint
+pkvxpeSessionInfo["f"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["d"]);
+pkvxpeSessionInfo["f"]["AspectRatio"] = 10.26923;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentDate
+pkvxpeSessionInfo["10"] = Instance.new("TextLabel", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["10"]["TextWrapped"] = true;
+pkvxpeSessionInfo["10"]["BorderSizePixel"] = 0;
+pkvxpeSessionInfo["10"]["TextSize"] = 14;
+pkvxpeSessionInfo["10"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+pkvxpeSessionInfo["10"]["TextScaled"] = true;
+pkvxpeSessionInfo["10"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["10"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+pkvxpeSessionInfo["10"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+pkvxpeSessionInfo["10"]["BackgroundTransparency"] = 1;
+pkvxpeSessionInfo["10"]["Size"] = UDim2.new(0.9639, 0, 0.13402, 0);
+pkvxpeSessionInfo["10"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+pkvxpeSessionInfo["10"]["Text"] = [[Date:]];
+pkvxpeSessionInfo["10"]["Name"] = [[currentDate]];
+pkvxpeSessionInfo["10"]["Position"] = UDim2.new(0.03249, 0, 0.58763, 0);
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentDate.LocalScript
+pkvxpeSessionInfo["11"] = Instance.new("LocalScript", pkvxpeSessionInfo["10"]);
+
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentDate.UIAspectRatioConstraint
+pkvxpeSessionInfo["12"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["10"]);
+pkvxpeSessionInfo["12"]["AspectRatio"] = 10.26923;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.UIAspectRatioConstraint
+pkvxpeSessionInfo["13"] = Instance.new("UIAspectRatioConstraint", pkvxpeSessionInfo["2"]);
+pkvxpeSessionInfo["13"]["AspectRatio"] = 1.42784;
+
+
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrDisplay.LocalScript
+local function C_6()
+local script = pkvxpeSessionInfo["6"];
+	script.Parent.Text = game.Players.LocalPlayer.DisplayName
+end;
+task.spawn(C_6);
+-- StarterGui.pkvxpeNewSessionInfo.Frame.plrName.LocalScript
+local function C_9()
+local script = pkvxpeSessionInfo["9"];
+	script.Parent.Text = "@" .. game.Players.LocalPlayer.Name
+end;
+task.spawn(C_9);
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentTime.LocalScript
+local function C_e()
+local script = pkvxpeSessionInfo["e"];
+	while true do
+		script.Parent.Text = "Time: "..os.date("%H:%M:%S")
+		wait(1)
+	end
+end;
+task.spawn(C_e);
+-- StarterGui.pkvxpeNewSessionInfo.Frame.currentDate.LocalScript
+local function C_11()
+local script = pkvxpeSessionInfo["11"];
+	script.Parent.Text = "Date: "..os.date("%Y-%m-%d")
+end;
+task.spawn(C_11);
+
+return pkvxpeSessionInfo["1"], require;
